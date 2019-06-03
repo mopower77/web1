67712b0b3c1ae11df61b28ac89b10c244336c346
mopower77 <mopower77@gmail.com>
2019-06-04 01:27:02 +0900

    commit

--- js/GameSelection.js
+++ js/GameSelection.js
@@ -4,24 +4,64 @@
 function Cross(type) {
     Title(type);
     RemoveScroll();
-}
+    RemoveScroll();
+    var parent_node = AddParentNode();
 
+    for (var i = 0; i < 50; ++i) {
+        HeaderData("종목","나라","리그");
+        WinDrawLoseBet();
+        HandiCap();
+        UnderOver();
+    }
+}
 
 function WindrawLose(type) {
     Title(type);
     RemoveScroll();
+    var parent_node = AddParentNode();
+
+    for (var i = 0; i < 50; ++i) {
+        HeaderData("종목","나라","리그");
+        WinDrawLoseBet();
+    }
+}
+
+function HandiOver(type) {
+    Title(type);
+    RemoveScroll();
+    var parent_node = AddParentNode();
+
+    for (var i = 0; i < 50; ++i) {
+        HeaderData("종목","나라","리그");
+        HandiCap();
+        UnderOver();
+    }
+}
+
+
+function Special(type) {
+    Title(type);
+    RemoveScroll();
+}
+
+function Live(type) {
+    Title(type);
+    RemoveScroll();
+}
+
+
+function AddParentNode()
+{
     var parent_node = document.createElement('div');
     parent_node.id = "parent_node";
     document.getElementById('scroll_Field').appendChild(parent_node);
-
-    for (var i = 0; i < 10; ++i) {
-        HeaderData("종목","나라","리그");
-    }
+    return parent_node;
 }
 
 function OpenGameSelection()
 {
     document.getElementById('club_name').innerText = "스페셜테스트쿨럽";
+    Title('Cross');
 }
 
 
@@ -42,14 +82,53 @@ function HeaderData(sports,county,league)
 
 function WinDrawLoseBet()
 {
-    
+    var div = document.createElement('div');
+    div.appendChild(ADdTeamInfo("홈팀"));
+    div.appendChild(Rate("win","승"));
+    div.appendChild(Rate("draw","무"));
+    div.appendChild(Rate("lose","패"));
+    div.appendChild(ADdTeamInfo("원정팀"));
+    parent_node.appendChild(div);
 }
 
 
-
-function HandiOverBet()
+function HandiCap()
 {
+    var div = document.createElement('div');
+    div.appendChild(ADdTeamInfo("홈팀"));
+    div.appendChild(Rate("Handi_win","핸디 승"));
+    div.appendChild(Rate("special_value","기준점"));
+    div.appendChild(Rate("Handi_lose","핸디 패"));
+    div.appendChild(ADdTeamInfo("원정팀"));
+    parent_node.appendChild(div);
+}
 
+function UnderOver()
+{
+    var div = document.createElement('div');
+    div.appendChild(ADdTeamInfo("홈팀"));
+    div.appendChild(Rate("under","언더"));
+    div.appendChild(Rate("special_value","기준점"));
+    div.appendChild(Rate("over","오버"));
+    div.appendChild(ADdTeamInfo("원정팀"));
+    parent_node.appendChild(div);
+}
+
+
+function Rate(id,key)
+{
+    var rate = document.createElement('span');
+    rate.id = id;
+    rate.innerText = key;
+    return rate;
+}
+
+function ADdTeamInfo(key)
+{
+    var team = document.createElement('span');
+    team.id = "team_name";
+    team.innerText = key;
+    return team;
 }
 
 
@@ -59,22 +138,6 @@ function RemoveScroll()
     var parent_node = document.getElementById('parent_node');
     if (parent_node != null)
         parent_node.remove();
-}
-
-function HandiOver(type) {
-    Title(type);
-    RemoveScroll();
-}
-
-
-function Special(type) {
-    Title(type);
-    RemoveScroll();
-}
-
-function Live(type) {
-    Title(type);
-    RemoveScroll();
 }
 
 function Title(type) {
--- js/Hudtop.js
+++ js/Hudtop.js
@@ -2,42 +2,37 @@
 
 var list_Page = new Array();
 
-function BackBtn()
-{
-    if(list_Page.length > 1)
-    {
+function BackBtn() {
+    if (list_Page.length > 1) {
         delete list_Page[list_Page.length - 1];
-        OpenPage(list_page[list_Page.length-1]);
+        OpenPage(list_page[list_Page.length - 1]);
     }
 
     return OpenPage("GameSelection");
 }
 
-function AddPage(key)
-{
+function AddPage(key) {
     list_Page[list_Page.length] = key;
 }
 
-function OpenPage(key)
-{
+function OpenPage(key) {
     AddPage(key);
 
-    switch(key)
-    {
+    switch (key) {
         case "GameSelection":
             window.location.href = "http://127.0.0.1:5500/htmls/GameSelection.html";
             break;
         case "MyBank":
-                window.location.href = "http://127.0.0.1:5500/htmls/MyBank.html";
-                break;
+            window.location.href = "http://127.0.0.1:5500/htmls/MyBank.html";
+            break;
         case "Message":
-                window.location.href = "http://127.0.0.1:5500/htmls/MyMessage.html";
-        break;
+            window.location.href = "http://127.0.0.1:5500/htmls/MyMessage.html";
+            break;
         case "BettingHistory":
-                window.location.href = "http://127.0.0.1:5500/htmls/BettingHistory.html";
-        break;
+            window.location.href = "http://127.0.0.1:5500/htmls/BettingHistory.html";
+            break;
         case "ItemShop":
-                window.location.href = "http://127.0.0.1:5500/htmls/ItemShop.html";
+            window.location.href = "http://127.0.0.1:5500/htmls/ItemShop.html";
             break;
     }
 }
